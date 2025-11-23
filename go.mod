4d6597e0b28a6cb860fa717cf0507cbbac26af47
Isaac Trugilho <trugilhoisaac@gmail.com>
2025-11-23 14:13:47 +0000

    atualizar projeto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Isaac002c/go-orientacao-a-objetos
+
+go 1.25.4
--- main.go
+++ main.go
@@ -3,24 +3,43 @@ package main
 import "fmt"
 
 type ContaCorrente struct {
-    titular string
-    saldo   float64
+	titular string
+	saldo   float64
+}
+
+type contaDaSilvia struct {
+	titular string
+	saldo   float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-    if valorDoSaque > 0 && valorDoSaque <= c.saldo {
-        c.saldo -= valorDoSaque
-        return "Saque realizado com sucesso"
-    } else {
-        return "Saldo insuficiente"
-    }
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) string{
+	podeDepositar := valorDoDeposito >= 0
+	if podeDepositar {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso"
+	} else {
+		return "Valor de depósito inválido"
+	}
 }
 
 func main() {
-    contaDaSilvia := ContaCorrente{
-        titular: "Silvia",
-        saldo:   500,
-    }
+	contaDaSilvia := ContaCorrente{
+		titular: "Silvia",
+		saldo:   500,
+	}
 
-    fmt.Println(contaDaSilvia.saldo) // Imprime o saldo inicial da conta
+	fmt.Println(contaDaSilvia.saldo)
+	contaDaSilvia.Depositar(-1000)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDaSilvia.Sacar(1200))
 }
